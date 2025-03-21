16f6b0517b3437253b1b1ac03d371ffb955b22bf
reraru505 <fnln504@gmail.com>
2025-03-22 01:57:32 +0530

    empty

--- registration_renewal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CompanyDetails, MemberDetails, DocumentDetails
-
-# Register the models for the admin panel
-admin.site.register(CompanyDetails)
-admin.site.register(MemberDetails)
-admin.site.register(DocumentDetails)
--- salesman_barman/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import SalesmanBarman
-from rest_framework import serializers
-
-class SalesmanBarmanSerializer (serializers.ModelSerializer): 
-    class Meta:
-        model = SalesmanBarman
-        fields = '__all__'
-        
