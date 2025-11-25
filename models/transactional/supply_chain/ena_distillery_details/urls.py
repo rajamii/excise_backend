95b688b0b31d66fae454d1e6938e63f8f51a9764
ishwarsharma <32608522+ishwarsharma@users.noreply.github.com>
2025-11-25 10:45:37 +0530

    24-11-2025 changes

--- models/transactional/supply_chain/ena_distillery_details/urls.py
+++ models/transactional/supply_chain/ena_distillery_details/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 app_name = 'ena_distillery_details'
+
 urlpatterns = [
     path('ena-distillery-types/', views.enaDistilleryTypesListAPIView.as_view(), name='ena-distillery-types-list'),
 ]
