94382ee7b9d185ede4f0d1a91aebd9ae6ef61d10
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2026-01-28 07:05:26 +0530

    brand ml per cases added

--- models/masters/supply_chain/transit_permit/models.py
+++ models/masters/supply_chain/transit_permit/models.py
@@ -13,3 +13,18 @@ class TransitPermitBottleType(models.Model):
 
     def __str__(self):
         return self.bottle_type
+
+class BrandMlInCases(models.Model):
+    ml = models.IntegerField(db_column='ml', unique=True)
+    pieces_in_case = models.IntegerField(db_column='pieces_in_case')
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'brand_ml_in_cases'
+        unique_together = (('ml', 'pieces_in_case'),)
+        verbose_name = 'Brand ML in Cases'
+        verbose_name_plural = 'Brand ML in Cases'
+
+    def __str__(self):
+        return f"{self.ml} ml - {self.pieces_in_case} pieces"
