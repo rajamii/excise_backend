63ad604ee92e97d0c212d88772c3bad325b63e7f
ITSALREADYBROKEN <sameerkhan432108765@gmail.com>
2026-05-19 20:25:39 +0530

    brand updated

---
+++ models/masters/supply_chain/transit_permit/migrations/0002_reset_brand_ml_in_cases_id_seq.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('transit_permit', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            -- Fix Postgres sequence drift when rows were inserted manually.
+            SELECT setval(
+                pg_get_serial_sequence('brand_ml_in_cases', 'id'),
+                COALESCE((SELECT MAX(id) FROM brand_ml_in_cases), 0),
+                true
+            );
+            """,
+            reverse_sql=migrations.RunSQL.noop,
+        ),
+    ]
+
