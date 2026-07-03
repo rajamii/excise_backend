7db9b8e9167b94d770db2ffa2c1eb7605a09fcfe
ITSALREADYBROKEN <sameerkhan432108765@gmail.com>
2026-07-03 19:27:53 +0530

    CRUD CREATED

--- models/masters/core/serializers/subdivision_serializer.py
+++ models/masters/core/serializers/subdivision_serializer.py
@@ -22,10 +22,7 @@ class SubdivisionSerializer(serializers.ModelSerializer):
         ]
         extra_kwargs = {
             'subdivision_code': {
-                'validators': [
-                    MinValueValidator(1000),
-                    MaxValueValidator(9999)
-                ]
+                'validators': []
             }
         }
 
