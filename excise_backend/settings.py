618344fcd959548addbb67ba4c65c48f1ecdc96b
Raj Amii <110229297+rajamii@users.noreply.github.com>
2026-07-06 13:40:39 +0530

    Update settings.py

--- excise_backend/settings.py
+++ excise_backend/settings.py
@@ -157,9 +157,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sems_db',       # Database name
+        'NAME': 'eAbkari_db',       # Database name
         'USER': 'postgres',         # Your PostgreSQL username
-        'PASSWORD': 'sameer123',  # Your PostgreSQL password
+        'PASSWORD': 'postgres',  # Your PostgreSQL password
         'HOST': 'localhost',        
         'PORT': '5432',             # Default PostgreSQL port
         'CONN_MAX_AGE': 300,          # Don't reuse connections — avoids aborted transaction state
