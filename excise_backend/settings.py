8b6e873b873bfc47f02810016a0dc8f88834fbd3
ITSALREADYBROKEN <sameerkhan432108765@gmail.com>
2026-07-21 19:37:42 +0530

    Revert "Merge branch 'yeting' into sameer"
    This reverts commit a92c113bf1c2efc7c648aec46a22130db5eecbf6, reversing
    changes made to 88bc5937440b0222bf439e80794bd375a90ee049.

--- excise_backend/settings.py
+++ excise_backend/settings.py
@@ -86,7 +86,6 @@ INSTALLED_APPS = [
     'models.transactional.company_collaboration',
     'models.transactional.license_renewal_application',
     'models.transactional.special_permit',
-    'models.transactional.distributor_permit',
     'models.transactional.site_enquiry',
     'models.transactional.new_license_application',
     'models.transactional.label_registration',
@@ -159,11 +158,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Sems_db',       # Database name
+        'NAME': 'sems_db',       # Database name
         'USER': 'postgres',         # Your PostgreSQL username
-        'PASSWORD': 'postgres',  # Your PostgreSQL password
+        'PASSWORD': 'sameer123',  # Your PostgreSQL password
         'HOST': 'localhost',        
-        'PORT': '5433',             # Default PostgreSQL port
+        'PORT': '5432',             # Default PostgreSQL port
         'CONN_MAX_AGE': 300,         # Don't reuse connections — avoids aborted transaction state
     }
 }
